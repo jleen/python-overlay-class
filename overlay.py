0b0b16a80cb4301039a7a9559a7baf00e7331a0b
John Leen <jleen@saturnvalley.org>
2026-01-31 14:33:24 -0500

    Avoid diamond inheritance on multiple overlays

--- overlay.py
+++ overlay.py
@@ -13,46 +13,35 @@ def overlay(overlay_class, base_object):
     All other attribute accesses (including assignments) will be
     delegated to the wrapped object.'''
 
-    # Three parent classes? Yes, three.
+    # The wrapper object needs to have two parent classes:
     #   1. the overlay class, for access to the new methods
-    #   2. _Overlay, the "engine" that delegates to the underlying object
-    #   3. the original class of the base object, so that we conform to it
+    #   2. the original class of the base object, so that we conform to it
     #      (otherwise, for example, super() from a method of the base object
     #      wouldn't work when called from an overlay)
-    class Overlaid(overlay_class, _Overlay, base_object.__class__):
-        pass
-    return Overlaid(base_object)
+    class Overlaid(overlay_class, base_object.__class__):
+        # Need to avoid re-running the underlying object's initializer.
+        def __init__(self):
+            pass
 
+        def __getattr__(self, name):
+            # Methods defined in the overlay will have already been resolved
+            # by the time we get here. So, anything else should just be delegated
+            # to the wrapped object.
+            #
+            # Note that we can't just allow multiple inheritance to take care of
+            # this, because we need to delegate to the original *object*,
+            # not just the original *class*.
+            value = getattr(base_object, name)
+            if type(value) == MethodType:
+                # If we're getting a method, it's bound to the base object.
+                # We need to rebind it so that its 'self' will point to us,
+                # and be able to see our overlay methods.
+                return value.__func__.__get__(self)
+            else:
+                return value
 
-class _Overlay:
-    '''A mix-in that implements the guts of the overlay mechanism.
+        def __setattr__(self, name, value):
+            # All writes should just go through to the wrapped object.
+            setattr(base_object, name, value)
 
-    An instance of this class itself does nothing useful. It is meant
-    to be combined with other classes via the "overlay" function above.'''
-
-    def __init__(self, base):
-        # Go through object.__setattr__ to avoid calling our magic setattr,
-        # since we don't want to delegate the "base" attribute.
-        # (In fact it would be an infinite recursion at this point!)
-        object.__setattr__(self, 'base', base)
-
-    def __getattr__(self, name):
-        # Methods defined in the overlay will have already been resolved
-        # by the time we get here. So, anything else should just be delegated
-        # to the wrapped object.
-        #
-        # Note that we can't just allow multiple inheritance to take care of
-        # this, because we need to delegate to the original *object*,
-        # not just the original *class*.
-        value = getattr(self.base, name)
-        if type(value) == MethodType:
-            # If we're getting a method, it's bound to the base object.
-            # We need to rebind it so that its 'self' will point to us,
-            # and be able to see our overlay methods.
-            return value.__func__.__get__(self)
-        else:
-            return value
-
-    def __setattr__(self, name, value):
-        # All writes should just go through to the wrapped object.
-        setattr(self.base, name, value)
+    return Overlaid()
